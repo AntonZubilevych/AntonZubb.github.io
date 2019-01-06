1b7b0dfc7482e994527500d0ac64d8b4e2ff1104
AntonZubilevych <antonzubb@gmail.com>
2019-01-06 18:06:00 +0200

    Initial commit

--- main.js
+++ main.js
@@ -13,23 +13,58 @@ function closeNav() {
 }
 
 function animatedBlock(){
-    var blocks = document.getElementById('inner_blocks').children;
-    for(var i = 0 ; i<blocks.length ; i++){
-        setTimeout(function (y) {
-            console.log(blocks[y]);
-            blocks[y].classList.add('show');
-            blocks[y].classList.add('zoomIn');
-        },i*1000,i);
+
+
+        var blocks = document.getElementById('inner_blocks').children;
+        for (var i = 0; i < blocks.length; i++) {
+            setTimeout(function (y) {
+                blocks[y].classList.add('show');
+                blocks[y].classList.add('zoomIn');
+            }, i * 500, i);
+        }
+}
+
+function bindAnimation() {
+    window.onscroll = function() {
+        var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        var width  = document.getElementById('credo').offsetTop;
+        var navbar = document.getElementsByClassName('pos-f-t')[0];
+        if(scrolled>width){
+            animatedBlock();
+        }
+
+        if(scrolled>10){
+            navbar.classList.add('opacity');
+        }else{
+            navbar.classList.remove('opacity');
+        }
     }
 }
 
+function animateCircle() {
+    var circle = document.getElementsByClassName('circle')[0];
 
-var options = {
-    strings: ["","Честність","Довіра","Доступність","Ваш Адвокат <br> Катерина Панчелюга"],
-    typeSpeed: 30
-};
 
-var typed = new Typed(".element", options);
+    circle.classList.add('show');
+    circle.classList.add('zoomInRight');
+    circle.addEventListener('mouseout' , function () {
+        circle.classList.remove('zoomInRight');
+        circle.classList.toggle('wobble');
 
-animatedBlock();
+    })
+}
+
+function typping() {
+    var options = {
+        strings: ["","Честність","Довіра","Доступність","Ваш Адвокат <br> Катерина Панчелюга"],
+        typeSpeed: 30
+    };
+
+    var typed = new Typed(".element", options);
+}
+
+typping();
+setTimeout(animateCircle,5500);
+bindAnimation();
+//animatedBlock();
 
